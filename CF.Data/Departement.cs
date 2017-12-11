f91508f71d420fbe10577a47372d4d8bafffc36e
shbaieb <shbaieb@SAHAR-PC>
2017-12-11 10:36:26 +0100

    de

--- CF.Data/Departement.cs
+++ CF.Data/Departement.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CF.Data
 {
-  public   class Departement : BaseEntity
+  public   class Departement : BaseEntity /*datareop */
     {
         
             public string Name { get; set; }
